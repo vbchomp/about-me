c0f59c8dd74cb427d4a17d523d5551f22fc415d9
Heather B <vbchomp@gmail.com>
2021-04-28 20:57:01 -0700

    fixed if, else loop. was using if and else without and else if. works now

--- js/app.js
+++ js/app.js
@@ -10,23 +10,21 @@ alert('Good Day, ' + username + '! Want to learn a little about me? Please answe
 //Question 1
 let questionOne = prompt('Do I live in WA?').toLowerCase();
 //if loop
-if(questionOne === 'yes' || questionOne === 'y'){
+if (questionOne === 'yes' || questionOne === 'y') {
   console.log('questionOne:' + questionOne);
   alert('That\'s right!');
-} else(questionOne === 'no' || questionOne === 'n');{
-  console.log('questionOne:' + questionOne);
+} else {
   alert('Yep, I do!');
 }
 
 //Question 2
 let questionTwo = prompt('Do I like animals?').toLowerCase();
-
-//Trying an else
-if(questionTwo === 'no' || questionTwo === 'n'){
+//if loop
+if (questionTwo === 'no' || questionTwo === 'n') {
   console.log('questioneTwo:' + questionTwo);
   alert('Are you kidding me, ' + username + '? I love snuggling with the floofers!');
-} else(questionTwo === 'yes' || questionTwo === 'y');{
-  console.log('questionTwo:' + questionTwo);
+} else {
+  //console.log('questionTwo:' + questionTwo);
   alert(username + ', of course I do!');
 }
 
@@ -35,9 +33,9 @@ let questionThree = prompt('Do I like rain gardens?').toLowerCase();
 //if loop
 if(questionThree === 'yes' || questionThree === 'y'){
   console.log('questionThree:' + questionThree);
-  alert('You are totally right, ' + username + ', and I wan\'t to learn to make one in my yard.');
-} else(questionThree === 'no' || questionThree === 'n');{
-  console.log('questionThree:' + questionThree);
+  alert('You are totally right, ' + username + ', and I want to learn to make one in my yard.');
+} else {
+  //console.log('questionThree:' + questionThree);
   alert(username + ', of course I do!');
 }
 
@@ -47,8 +45,8 @@ let questionFour = prompt('Do I want to live on the East Coast?').toLowerCase();
 if(questionFour === 'yes' || questionFour === 'y'){
   console.log('questionFour:' + questionFour);
   alert('Syke! Guess again!');
-} else(questionFour === 'no' || questionFour === 'n');{
-  console.log('questionFour:' + questionFour);
+} else {
+  //console.log('questionFour:' + questionFour);
   alert('Once again, ' + username + ', you are good at this!');
 }
 
@@ -58,8 +56,8 @@ let questionFive = prompt('Have I lived in Europe?').toLowerCase();
 if(questionFive === 'no' || questionFive === 'n'){
   console.log('questionFive:' + questionFive);
   alert('Actually, ' + username + ', I was an Army Brat and spent many years in Germany and Italy. ');
-} else(questionFive === 'yes' || questionFive === 'y');{
-  console.log('questionFive:' + questionFive);
+} else {
+  //console.log('questionFive:' + questionFive);
   alert(username + ', you are good at this!');
 }
 
